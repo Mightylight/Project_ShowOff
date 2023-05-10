26a6725270bb5bde03be9c9fd983365db2ca4340
Alex Wojtowicz <izabella.wojtowicz@gmail.com>
2023-05-10 12:05:59 +0200

    rigidbody aligator movement

--- Assets/Scripts/Movement/ControllerMovement.cs
+++ Assets/Scripts/Movement/ControllerMovement.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
 public class ControllerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody rb;
+    [SerializeField] float speed = 5;
+    Vector3 inputs = Vector3.zero;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        turn();
+    }
+
+    private void FixedUpdate()
+    {
+        move();
+    }
+
+    void turn()
+    {
+        inputs = Vector3.zero;
+        inputs.x = Input.GetAxis("Horizontal2");
+        inputs.z = Input.GetAxis("Vertical2");
+
+        if(inputs != Vector3.zero)
+        {
+            transform.forward = inputs;
+        }
+
+
+    }
+
+    void move()
+    {
+        rb.MovePosition(rb.position + inputs * speed * Time.fixedDeltaTime);
     }
 }
