683b2d2e0d99743c26ca98e87453a32396c17f69
Alex Wojtowicz <izabella.wojtowicz@gmail.com>
2023-05-16 18:39:33 +0200

    canoe invincibility timer

--- Assets/Scripts/Canoe/CanoeManager.cs
+++ Assets/Scripts/Canoe/CanoeManager.cs
@@ -9,16 +9,31 @@ namespace Canoe
         [SerializeField] private HealthBar _healthBar;
 
         public UnityEvent loss;
+        private float _timer;
+        bool _isInvincible = false;
+        [SerializeField] private float _invincibilityTimer = 0.5f;
 
-
+        private void Update()
+        {
+            if (_isInvincible)
+            {
+                _timer -= Time.deltaTime;
+                if (_timer <= 0) _isInvincible = false;
+            }
+        }
         public void OnAlligatorHit()
         {
-            _health--;
-            _healthBar.TakeDamage(1);
-
-            if(_health <= 0)
+            if (!_isInvincible)
             {
-                loss?.Invoke();
+                _health--;
+                _healthBar.TakeDamage(1);
+
+                if (_health <= 0)
+                {
+                    loss?.Invoke();
+                }
+                _isInvincible = true;
+                _timer = _invincibilityTimer;
             }
         }
     }
