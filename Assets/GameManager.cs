dd4ab026bc2b6650f1e902842779691fd4bc0d52
Alex Wojtowicz <izabella.wojtowicz@gmail.com>
2023-05-16 18:36:03 +0200

    Works better!

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,17 +6,30 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     bool _gameOver = false;
+    [SerializeField] GameObject[] _destroyObjects;
+
+    void Start()
+    {
+        // Find all objects in the scene that are marked with DontDestroyOnLoad
+        
+
+    }
     void Update()
     {
         if (Input.anyKeyDown && _gameOver)
         {
             RestartScene();
         }
+
     }
 
     void RestartScene()
     {
         Scene currentScene = SceneManager.GetActiveScene();
+        foreach(GameObject gameObject in _destroyObjects)
+        {
+            Destroy(gameObject);
+        }
         SceneManager.LoadScene(currentScene.buildIndex);
     }
 
