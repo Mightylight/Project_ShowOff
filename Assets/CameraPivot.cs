5663c9e46b288f3ffdf933f4a27b9e4326b6635e
Alex Wojtowicz <izabella.wojtowicz@gmail.com>
2023-05-26 13:54:43 +0200

    INPUT MANAGER FFS

--- Assets/CameraPivot.cs
+++ Assets/CameraPivot.cs
@@ -13,9 +13,14 @@ public class CameraPivot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float speed = 3.0f;
-        float xRot = speed * Input.GetAxis("VerticalXbox");
-        float yRot = speed * Input.GetAxis("HorizontalXbox");
-        transform.Rotate(0, yRot, 0.0f);
+        //float speed = 3.0f;
+        //float xRot = speed * Input.GetAxis("VerticalXbox");
+        //float yRot = speed * Input.GetAxis("HorizontalXbox");
+        //transform.Rotate(0, yRot, 0.0f);
+
+        float sin = Input.GetAxis("HorizontalXbox");
+
+        float angle =Mathf.Asin(sin) *Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0, angle, 0);
     }
 }
