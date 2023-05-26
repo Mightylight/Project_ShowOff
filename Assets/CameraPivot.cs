fe3e96bc4e8819d769b5b4baeea18cf6f1aedc8e
Alex Wojtowicz <izabella.wojtowicz@gmail.com>
2023-05-26 14:39:42 +0200

    ncncm

--- Assets/CameraPivot.cs
+++ Assets/CameraPivot.cs
@@ -4,23 +4,37 @@ using UnityEngine;
 
 public class CameraPivot : MonoBehaviour
 {
+    [SerializeField] float upperXRotClamp;
+    [SerializeField] float lowerXRotClamp;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    float speed = 3.0f;
+    //    float xRot = speed * Input.GetAxis("VerticalXbox");
+    //    float yRot = speed * Input.GetAxis("HorizontalXbox");
+    //    transform.Rotate(xRot, yRot, 0.0f);
+    //    transform.eulerAngles.x = Mathf.Clamp(transform.eulerAngles.x, 0, 90);
+    //}
+
+    float currentRotation;
+
+    void SetCurrentRotation(float rot)
+    {
+        currentRotation = Mathf.Clamp(rot, -90, 90);
+        transform.rotation = Quaternion.Euler(rot, 0, 0);
+    }
+
     void Update()
     {
-        //float speed = 3.0f;
-        //float xRot = speed * Input.GetAxis("VerticalXbox");
-        //float yRot = speed * Input.GetAxis("HorizontalXbox");
-        //transform.Rotate(0, yRot, 0.0f);
-
-        float sin = Input.GetAxis("HorizontalXbox");
-
-        float angle =Mathf.Asin(sin) *Mathf.Rad2Deg;
-        transform.eulerAngles = new Vector3(0, angle, 0);
+        float newRotation = currentRotation + Input.GetAxis("VerticalXbox") * 3;
+        SetCurrentRotation(newRotation);
     }
+
 }
+
