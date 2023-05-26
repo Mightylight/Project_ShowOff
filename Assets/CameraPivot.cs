e06eb6dbd332826103cf24b8e3c782eeec44cb91
Alex Wojtowicz <izabella.wojtowicz@gmail.com>
2023-05-26 18:44:33 +0200

    camera pivot adjustements
    the gator strafes instead of turning for now - its because allowing turning would cause some merge conflicts because of the way the prefab is set up

--- Assets/CameraPivot.cs
+++ Assets/CameraPivot.cs
@@ -4,37 +4,42 @@ using UnityEngine;
 
 public class CameraPivot : MonoBehaviour
 {
-    [SerializeField] float upperXRotClamp;
-    [SerializeField] float lowerXRotClamp;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float minPitch;
+    [SerializeField] float maxPitch;
+    [SerializeField] float pitchSpeed;
+    float currentPitch;
+
+    [SerializeField] float yawSpeed;
+
+
+    void ChangePitch(float angle)
     {
         
+        transform.rotation = Quaternion.Euler(angle, 0, 0);
     }
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    float speed = 3.0f;
-    //    float xRot = speed * Input.GetAxis("VerticalXbox");
-    //    float yRot = speed * Input.GetAxis("HorizontalXbox");
-    //    transform.Rotate(xRot, yRot, 0.0f);
-    //    transform.eulerAngles.x = Mathf.Clamp(transform.eulerAngles.x, 0, 90);
-    //}
-
-    float currentRotation;
-
-    void SetCurrentRotation(float rot)
+    void ChangeYaw(float angle)
     {
-        currentRotation = Mathf.Clamp(rot, -90, 90);
-        transform.rotation = Quaternion.Euler(rot, 0, 0);
+        transform.rotation = Quaternion.Euler(currentPitch, angle, 0);
     }
 
     void Update()
     {
-        float newRotation = currentRotation + Input.GetAxis("VerticalXbox") * 3;
-        SetCurrentRotation(newRotation);
+        currentPitch = currentPitch + Input.GetAxis("VerticalXbox") * pitchSpeed;
+        currentPitch = Mathf.Clamp(currentPitch, minPitch, maxPitch);
+
+        float yaw = transform.eulerAngles.y + Input.GetAxis("HorizontalXbox") * yawSpeed;
+
+        transform.rotation = Quaternion.Euler(currentPitch, yaw, 0);
     }
 
 }
-
+//// Update is called once per frame
+//void Update()
+//{
+//    float speed = 3.0f;
+//    float xRot = speed * Input.GetAxis("VerticalXbox");
+//    float yRot = speed * Input.GetAxis("HorizontalXbox");
+//    transform.Rotate(xRot, yRot, 0.0f);
+//    transform.eulerAngles.x = Mathf.Clamp(transform.eulerAngles.x, 0, 90);
+//}
