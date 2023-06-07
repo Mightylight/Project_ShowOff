fe49ae552bae9c9a16027d42858aa223d6747cdc
rs189 <35667100+rs189@users.noreply.github.com>
2023-06-07 12:42:41 +0200

    Changed healthbar script, changed healthbar references

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -5,30 +5,39 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Slider healthSlider;
+    //public Slider healthSlider;
+    public Image healthFill;
+
+    private int maxHealth;
 
     private void Awake()
     {
-        healthSlider = GetComponent<Slider>();
+        //healthSlider = GetComponent<Slider>();
+        healthFill = GetComponentInChildren<Image>();
     }
 
     public void SetMaxHealth(int value, bool resetHealth)
     {
-        healthSlider.maxValue = value;
-        if (resetHealth) healthSlider.value = value;
+        //healthSlider.maxValue = value;
+        //if (resetHealth) healthSlider.value = value;
+        maxHealth = value;
+        healthFill.fillAmount = 1f;
     }
     public void SetHealth(int value)
     {
-        healthSlider.value = value;
+        //healthSlider.value = value;
+        healthFill.fillAmount = (float)value / maxHealth;
     }
 
     public void TakeDamage(int value)
     {
-        healthSlider.value -= value;
+        //healthSlider.value -= value;
+        healthFill.fillAmount -= (float)value / maxHealth;
     }
 
     public void GetHealed(int value)
     {
-        healthSlider.value += value;
+        //healthSlider.value += value;
+        healthFill.fillAmount += (float)value / maxHealth;
     }
 }
