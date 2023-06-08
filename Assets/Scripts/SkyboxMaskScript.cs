b9d35a777ba1e4606cac0fe9bb00e2746c3da821
rs189 <caminoonpc@gmail.com>
2023-06-08 14:29:55 +0200

    Changed lengths of the sections

--- Assets/Scripts/SkyboxMaskScript.cs
+++ Assets/Scripts/SkyboxMaskScript.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class SkyboxMaskScript : MonoBehaviour
 {
     [SerializeField] private Transform _canoe;
+
+    [SerializeField] private float _frontOffset = 0f;
+    [SerializeField] private float _backOffset = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
