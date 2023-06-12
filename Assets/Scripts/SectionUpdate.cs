767d19ccfb6521f356830cd46a0ceccf50f2f00b
rs189 <35667100+rs189@users.noreply.github.com>
2023-06-12 12:01:08 +0200

    Skybox blending script

---
+++ Assets/Scripts/SectionUpdate.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectionUpdate : MonoBehaviour
+{
+    // Skybox material
+    public Material skyboxMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If J key is pressed
+        
+        // if (Input.GetKeyDown(KeyCode.J))
+        // {
+        //     // Start coroutine
+        //     StartCoroutine(OnMidSkyboxFade());
+        // }
+    }
+
+    IEnumerator OnMidSkyboxFade()
+    {
+        // Get _BlendAmount property
+        float blendAmount = skyboxMaterial.GetFloat("_BlendAmount");
+        // Fade skybox from 0 to 0.5
+        while (blendAmount < 0.5f)
+        {
+            blendAmount += 0.01f;
+            skyboxMaterial.SetFloat("_BlendAmount", blendAmount);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    IEnumerator OnFinalSkyboxFade()
+    {
+        // Get _BlendAmount property
+        float blendAmount = skyboxMaterial.GetFloat("_BlendAmount");
+        // Fade skybox from 0.5 to 1
+        while (blendAmount < 1f)
+        {
+            blendAmount += 0.01f;
+            skyboxMaterial.SetFloat("_BlendAmount", blendAmount);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    public void OnMidSectionEnter()
+    {
+        StartCoroutine(OnMidSkyboxFade());
+    }
+
+    public void OnFinalSectionEnter()
+    {
+        StartCoroutine(OnFinalSkyboxFade());
+    }
+}
