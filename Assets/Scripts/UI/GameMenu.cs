a592dded3710d4b2015f5ca942f8b4cf85b051c1
rs189 <caminoonpc@gmail.com>
2023-06-28 16:06:41 +0200

    Main menu

--- Assets/Scripts/UI/GameMenu.cs
+++ Assets/Scripts/UI/GameMenu.cs
@@ -7,29 +7,44 @@ using UnityEngine.SceneManagement;
 
 public class GameMenu : MonoBehaviour
 {
-    //private JoystickControls joystickControls;
-    [SerializeField] private GameObject playButtonSelected;
-    [SerializeField] private GameObject settingsButtonSelected;
-    [SerializeField] private GameObject exitGameSelected;
-
     [SerializeField] private AudioManager audioManager;
 
     [SerializeField] private GameObject mainMenuUI;
     [SerializeField] private GameObject settingsMenuUI;
+    [SerializeField] private GameObject controlsUI;
     [SerializeField] private bool isMenuVisible = false;
+    [SerializeField] private bool isSettingsVisible = false;
 
+    // Main Menu menu
+    [Header("Main Menu")]
+    [SerializeField] private GameObject playButtonSelected;
+    [SerializeField] private GameObject settingsButtonSelected;
+    [SerializeField] private GameObject exitGameSelected;
+
+    // Settings Menu menu
+    [Header("Settings Menu")]
+    [SerializeField] private GameObject volumeMusicSelected;
+    [SerializeField] private GameObject volumeSoundSelected;
+    [SerializeField] private GameObject backSelected;
     [SerializeField] private Image soundVolume;
     [SerializeField] private Image musicVolume;
 
-    [SerializeField] public UnityEvent OnMainMenuButtonClick;
+    // Settings Menu menu
+    [Header("Loading Screen")]
+    [SerializeField] private string loadingSceneName = "Playtest";
+    [SerializeField] private GameObject loadingScreen;
 
-    private int currentSelectedIndex = 0;
+    private int currentMenuSelectedIndex = 0;
+    private int currentSettingsSelectedIndex = 0;
 
+    private bool buttonHeldDown = false;
     private bool joystickHeldDown = false;
     private bool joystickHeldUp = false;
     private bool joystickHeldLeft = false;
     private bool joystickHeldRight = false;
     private float joystickThreshold = 0.75f;
+
+    private bool isLoading = false;
 
     private JoystickControls _joystickControls;
 
@@ -38,27 +53,81 @@ public class GameMenu : MonoBehaviour
         _joystickControls = new JoystickControls();
 
         _joystickControls.Alligator.Bite.performed += pCtx => PressButton();
+        _joystickControls.Alligator.Bite.canceled += pCtx => buttonHeldDown = false;
+
         _joystickControls.Alligator.Enable();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        //joystickControls = new JoystickControls();
+    }
+
+    IEnumerator LoadAsyncScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(loadingSceneName);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 
     void PressButton()
     {
-        Debug.Log("PressButton");
+        if (buttonHeldDown || isLoading)
+        {
+            return;
+        }
+
+        buttonHeldDown = true;
+
+        switch (currentMenuSelectedIndex)
+        {
+            case 0:
+                mainMenuUI.SetActive(false);
+                settingsMenuUI.SetActive(false);
+                loadingScreen.SetActive(true);
+                controlsUI.SetActive(false);
+
+                isLoading = true;
+
+                //SceneManager.LoadScene(loadingSceneName);
+                StartCoroutine(LoadAsyncScene());
+
+                break;
+            case 1:
+                currentSettingsSelectedIndex = 0;
+                currentMenuSelectedIndex = 0;
+
+                mainMenuUI.SetActive(false);
+                settingsMenuUI.SetActive(true);
+                break;
+            case 2:
+                Application.Quit();
+                break;
+        }
+
+        if (!settingsMenuUI.activeSelf)
+        {
+            return;
+        }
+
+        switch (currentSettingsSelectedIndex)
+        {
+            case 2:
+                currentSettingsSelectedIndex = 0;
+                currentMenuSelectedIndex = 0;
+
+                mainMenuUI.SetActive(true);
+                settingsMenuUI.SetActive(false);
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log("Horizontal: " + Input.GetAxis("Horizontal"));
-        Debug.Log("Vertical: " + Input.GetAxis("Vertical"));
-
-        // If joystick moved down, move to next button.
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
 
@@ -71,75 +140,55 @@ public class GameMenu : MonoBehaviour
                 joystickHeldLeft = false;
                 joystickHeldRight = false;
 
-                if (currentSelectedIndex < 2)
+                if (currentMenuSelectedIndex < 2)
                 {
-                    currentSelectedIndex++;
+                    currentMenuSelectedIndex++;
                 }
                 else
                 {
-                    currentSelectedIndex = 0;
+                    currentMenuSelectedIndex = 0;
+                }
+
+                if (currentSettingsSelectedIndex < 2)
+                {
+                    currentSettingsSelectedIndex++;
+                }
+                else
+                {
+                    currentSettingsSelectedIndex = 0;
                 }
             }
         }
         else if (verticalInput > joystickThreshold)
-    {
-        if (!joystickHeldUp)
         {
-            joystickHeldUp = true;
-            joystickHeldDown = false;
-            joystickHeldLeft = false;
-            joystickHeldRight = false;
-
-            if (currentSelectedIndex > 0)
+            if (!joystickHeldUp)
             {
-                currentSelectedIndex--;
-            }
-            else
-            {
-                    currentSelectedIndex = 2;
-                }
-            }
-        }
-        else if (horizontalInput < -joystickThreshold)
-        {
-            if (!joystickHeldLeft)
-            {
-                joystickHeldLeft = true;
+                joystickHeldUp = true;
                 joystickHeldDown = false;
-                joystickHeldUp = false;
+                joystickHeldLeft = false;
                 joystickHeldRight = false;
 
-                if (currentSelectedIndex == 0)
+                if (currentMenuSelectedIndex > 0)
                 {
-                    OnMusicVolumeLeft();
+                    currentMenuSelectedIndex--;
                 }
-                else if (currentSelectedIndex == 1)
+                else
                 {
-                    OnSoundVolumeLeft();
+                    currentMenuSelectedIndex = 2;
                 }
-            }
-        }
-        else if (horizontalInput > joystickThreshold)
-        {
-            if (!joystickHeldRight)
-            {
-                joystickHeldRight = true;
-                joystickHeldDown = false;
-                joystickHeldUp = false;
-                joystickHeldLeft = false;
 
-                if (currentSelectedIndex == 0)
+                if (currentSettingsSelectedIndex > 0)
                 {
-                    OnMusicVolumeRight();
+                    currentSettingsSelectedIndex--;
                 }
-                else if (currentSelectedIndex == 1)
+                else
                 {
-                    OnSoundVolumeRight();
+                    currentSettingsSelectedIndex = 2;
                 }
             }
         }
         else if (verticalInput >= -joystickThreshold && verticalInput <= joystickThreshold
-            && horizontalInput >= -joystickThreshold && horizontalInput <= joystickThreshold)
+                && horizontalInput >= -joystickThreshold && horizontalInput <= joystickThreshold)
         {
             joystickHeldDown = false;
             joystickHeldUp = false;
@@ -147,8 +196,8 @@ public class GameMenu : MonoBehaviour
             joystickHeldRight = false;
         }
 
-        // Switch currentSelectedIndex
-        switch (currentSelectedIndex)
+        // Switch currentMenuSelectedIndex
+        switch (currentMenuSelectedIndex)
         {
             case 0:
                 playButtonSelected.SetActive(true);
@@ -166,20 +215,65 @@ public class GameMenu : MonoBehaviour
                 exitGameSelected.SetActive(true);
                 break;
         }
-    }
 
-    void HideMenu()
-    {
-        mainMenuUI.SetActive(true);
-        //Time.timeScale = 0f;
-        isMenuVisible = true;
-    }
+        if (horizontalInput < -joystickThreshold)
+        {
+            if (!joystickHeldLeft)
+            {
+                joystickHeldLeft = true;
+                joystickHeldDown = false;
+                joystickHeldUp = false;
+                joystickHeldRight = false;
 
-    void ShowMenu()
-    {
-        mainMenuUI.SetActive(false);
-        //Time.timeScale = 1f;
-        isMenuVisible = false;
+                if (currentSettingsSelectedIndex == 0)
+                {
+                    OnMusicVolumeLeft();
+                }
+                else if (currentSettingsSelectedIndex == 1)
+                {
+                    OnSoundVolumeLeft();
+                }
+            }
+        }
+        else if (horizontalInput > joystickThreshold)
+        {
+            if (!joystickHeldRight)
+            {
+                joystickHeldRight = true;
+                joystickHeldDown = false;
+                joystickHeldUp = false;
+                joystickHeldLeft = false;
+
+                if (currentSettingsSelectedIndex == 0)
+                {
+                    OnMusicVolumeRight();
+                }
+                else if (currentSettingsSelectedIndex == 1)
+                {
+                    OnSoundVolumeRight();
+                }
+            }
+        }
+
+        // Switch currentSettingsSelectedIndex
+        switch (currentSettingsSelectedIndex)
+        {
+            case 0:
+                volumeMusicSelected.SetActive(true);
+                volumeSoundSelected.SetActive(false);
+                backSelected.SetActive(false);
+                break;
+            case 1:
+                volumeMusicSelected.SetActive(false);
+                volumeSoundSelected.SetActive(true);
+                backSelected.SetActive(false);
+                break;
+            case 2:
+                volumeMusicSelected.SetActive(false);
+                volumeSoundSelected.SetActive(false);
+                backSelected.SetActive(true);
+                break;
+        }
     }
 
     // Whenever the joystick is moved left, the music volume is reduced.
@@ -226,10 +320,5 @@ public class GameMenu : MonoBehaviour
             soundVolume.fillAmount += 0.1f;
             //audioManager.SetSoundVolume(soundVolume.fillAmount);
         }
-    }
-
-    public void MainMenuButtonClick()
-    {
-        SceneManager.LoadScene("MainMenu");
     }
 }
