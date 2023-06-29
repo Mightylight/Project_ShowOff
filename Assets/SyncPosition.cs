92e1d7687d848240ab9e2dc5fad113d34e24e18b
Alex Wojtowicz <izabella.wojtowicz@gmail.com>
2023-06-29 13:25:55 +0200

    John Johnstonson Johnson William John Julian Junior IIIXIV

--- Assets/SyncPosition.cs
+++ Assets/SyncPosition.cs
@@ -6,8 +6,10 @@ public class SyncPosition : MonoBehaviour
 {
     [SerializeField] Transform _syncTo;
     [SerializeField] private bool _isRotating = true;
+    [SerializeField] private bool _isMoving = true;
     [SerializeField] private bool _VRRot = false;
     [SerializeField] float speed = 0.8f;
+    [SerializeField] private bool _onlyYrot = false;
 
 
     private void Update()
@@ -16,9 +18,13 @@ public class SyncPosition : MonoBehaviour
     }
     void Sync()
     {
-        transform.position = _syncTo.position;
+        if (_isMoving) transform.position = _syncTo.position;
         if (_isRotating)
         {
+            if (_onlyYrot)
+            {
+                transform.rotation = Quaternion.Euler(0, _syncTo.rotation.y, 0);
+            }
             transform.rotation = _syncTo.rotation;
         }
         else if (_VRRot)
